8c3ccf17205e33d7b386415846e39a76f5924bd5
venkat gadiyakari <venkatgadiyakari@192.168.1.8>
2025-03-11 22:34:29 +0530

    updating guest profile using server actions

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,4 +1,3 @@
-import { ca } from "date-fns/locale";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { createGuest, getGuest } from "./data-service";
@@ -14,25 +13,25 @@ const authConfig = {
     authorized({ auth, request }) {
       return !!auth?.user;
     },
+    async signIn({ user, account, profile }) {
+      try {
+        const exisitingGuest = await getGuest(user.email);
+        if (!exisitingGuest) {
+          await createGuest({ email: user.email, fullName: user.name });
+        }
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
   },
   pages: {
     signIn: "/login",
-  },
-  async signIn({ user, account, profile }) {
-    try {
-      const exisitingGuest = await getGuest(user.email);
-      if (!exisitingGuest) {
-        await createGuest({ email: user.email, fullName: user.name });
-      }
-      return true;
-    } catch {
-      return false;
-    }
-  },
-  async session({ session, user }) {
-    const guest = await getGuest(session.user.email);
-    session.user.guestId = guest.id;
-    return session;
   },
 };
 
